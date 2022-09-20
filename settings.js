051da1b062a506f81cd1f477ea5b4f34e00ca263
Hjdhhsh70 <113865164+Hjdhhsh70@users.noreply.github.com>
2022-09-20 07:36:47 +0200

    settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['201288455338'] //ur owner number
+global.vcardowner = ['249963346984'] //ur owner number
 global.ownername = "Youssef Al Soltan" //ur owner name
 global.ytname = "YT: Youssef-AlSoltan" //ur yt chanel name
 global.socialm = "Insta: @1y._.o2" //ur github or insta name
 global.location = "Egypt, Al Behera, Kafr Al dwar" //ur location
 
 //bot bomdy 
-global.owner = ['201288455338']
-global.ownertag = '201288455338' //ur tag number
-global.botname = 'بوت السلطان|bot alsoltan' //ur bot name
-global.linkz = "https://youtube.com/channel/UCf57gAihG_YlH7Rj-gbKuHA" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtube.com/channel/UCf57gAihG_YlH7Rj-gbKuHA" //ur website to be displayed
-global.botscript = 'https://chat.whatsapp.com/FiRwXZoBuSyLlUZBtNAgx8' //script link
+global.owner = ['24993944651']
+global.ownertag = '24993944651' //ur tag number
+global.botname = 'بوت بسبوس |bot alsoltan' //ur bot name
+global.linkz = "https://youtube.com/channel/UC_htYMALWjN7Orl3t5edumg" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UC_htYMALWjN7Orl3t5edumg" //ur website to be displayed
+global.botscript = 'https://chat.whatsapp.com/I2t980Jrp3zEjVum6kdsmU' //script link
 global.reactmoji = "💻" //ur menu react emoji
 global.themeemoji = "💎" //ur theme emoji
-global.packname = "By Youssef Al Soltan" //ur sticker watermark packname
-global.author = "By @201288455338" //ur sticker watermark author
+global.packname = "By 𝐏𝐒𝐏𝐎𝐎𝐒𝐀‏⃤‏༼͢🇦🇱 Al Soltan" //ur sticker watermark packname
+global.author = "By @249963346984" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['201288455338'] //ur premium numbers
+global.premium = ['249963346984'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
